6f5b08e9348f53ef783cf80c1091c061af994e55
BigMuscleMann <zanekg123@gmail.com>
2021-11-23 13:33:04 -0600

    commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // require is provided by loader.min.js.
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' } });
 require(["vs/editor/editor.main"], function() {
-  editor = monaco.editor.create(document.getElementById('code'), {
-    value: `function x() {
+    editor = monaco.editor.create(document.getElementById('code'), {
+        value: `function x() {
   console.log("Hello world!");
 }`,
-    language: 'javascript',
-    theme: 'vs-dark',
-    fontSize: "20px"
-  });
+        language: 'javascript',
+        theme: 'vs-dark',
+        fontSize: "20px",
+        horizontal: "visible",
+    });
 });
 
 
@@ -17,15 +18,14 @@ require(["vs/editor/editor.main"], function() {
 // require is provided by loader.min.js.
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' } });
 require(["vs/editor/editor.main"], function() {
-  editor1 = monaco.editor.create(document.getElementById('code1'), {
-    value: 
-`<div class="container">
+    editor1 = monaco.editor.create(document.getElementById('code1'), {
+        value: `<div class="container">
     <h1>Hello World!</h1>
 </div>`,
-    fontSize: "20px",
-    language: 'html',
-    theme: 'vs-dark',
-  });
+        fontSize: "20px",
+        language: 'html',
+        theme: 'vs-dark',
+    });
 });
 
 
@@ -35,61 +35,59 @@ require(["vs/editor/editor.main"], function() {
 // require is provided by loader.min.js.
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' } });
 require(["vs/editor/editor.main"], function() {
-   editor2 = monaco.editor.create(document.getElementById('code2'), {
-    value: `*{
+    editor2 = monaco.editor.create(document.getElementById('code2'), {
+        value: `*{
       margin: 0;
       font-family: Arial, sans-serif;
 }`,
-    language: 'css',
-    fontSize: "20px",
-    theme: 'vs-dark',
-  });
+        language: 'css',
+        fontSize: "20px",
+        theme: 'vs-dark',
+    });
 });
 
-function hide(which, which1, which2){
-  document.getElementById(which).style.display = 'none';
-  document.getElementById(which1).style.display = 'none';
-  document.getElementById(which2).style.display = 'none';
+
+function hide(which, which1, which2) {
+    document.getElementById(which).style.display = 'none';
+    document.getElementById(which1).style.display = 'none';
+    document.getElementById(which2).style.display = 'none';
 }
 
-function show(which){
-  document.getElementById(which).style.display = 'block'
+function show(which) {
+    document.getElementById(which).style.display = 'block'
 }
 
 
-function showTab(which){
-  if (which === 'code'){
-    hide('code1', 'code2', 'code3')
-    show(which)
+function showTab(which) {
+    if (which === 'code') {
+        hide('code1', 'code2', 'code3')
+        show(which)
 
-    document.getElementById('displayLang').innerText = 'Language: JS'
+        document.getElementById('displayLang').innerText = 'Language: JS'
 
-    window.location.hash='Javascript';
-  }
-  else if (which === 'code1'){
-    hide('code', 'code2', 'code3')
-    show(which)    
+        window.location.hash = 'Javascript';
+    } else if (which === 'code1') {
+        hide('code', 'code2', 'code3')
+        show(which)
 
-    document.getElementById('displayLang').innerText = 'Language: HTML'
+        document.getElementById('displayLang').innerText = 'Language: HTML'
 
-    window.location.hash='HTML';
-  }
-  else if (which === 'code2'){
-    hide('code', 'code1', 'code3')
-    show(which)
+        window.location.hash = 'HTML';
+    } else if (which === 'code2') {
+        hide('code', 'code1', 'code3')
+        show(which)
 
-    document.getElementById('displayLang').innerText = 'Language: CSS'
+        document.getElementById('displayLang').innerText = 'Language: CSS'
 
-    window.location.hash='CSS';
-  }
-    else if (which === 'code3'){
-      hide('code', 'code1', 'code2')
-      show(which)
-  
-      document.getElementById('displayLang').innerText = 'Output'
-  
-      window.location.hash='Output';
-  }
+        window.location.hash = 'CSS';
+    } else if (which === 'code3') {
+        hide('code', 'code1', 'code2')
+        show(which)
+
+        document.getElementById('displayLang').innerText = 'Output'
+
+        window.location.hash = 'Output';
+    }
 }
 
 
@@ -103,9 +101,9 @@ Outputs and Local Storage:
 
 
 function updateIframe() {
-  var myFrame = $("#output").contents().find('body');
-  var textareaValue = 
-`
+    var myFrame = $("#output").contents().find('body');
+    var textareaValue =
+        `
 <html>
   <head>
     <style>
@@ -120,11 +118,11 @@ function updateIframe() {
   </body>
 </html>
 `
-  myFrame.html(textareaValue);
+    myFrame.html(textareaValue);
 };
 
 document.onkeyup = function(e) {
- if (e.ctrlKey && e.altKey && e.which == 83) {
-    updateIframe()
-  }
+    if (e.ctrlKey && e.altKey && e.which == 83) {
+        updateIframe()
+    }
 };
